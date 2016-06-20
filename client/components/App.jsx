6849b6a83d7ad4f953896bbf41a8283f5cfaf611
Devon McGrath <devon.mcgrath@gmail.com>
2016-06-20 18:07:21 +1200

    refactored so wireframe is not in Header.jsx (woops)

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,6 +9,26 @@ export default React.createClass({
     return (
           <div id='app-container'>
             <Header />
+            <div id='todo_container'>
+                <h2>To Do List:</h2>
+                <table>
+                    <tr>
+                        <th>placeholder</th>
+                    </tr>
+                    <tr>
+                        <td>heres an item</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div id='weather_container'>
+                <h2>Weather API</h2>
+                <p>it'll go here</p>
+            </div>
+
+            <div class='footer'>
+                <p>artist info</p>
+            </div>
           </div>
       )
     }
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,28 +5,10 @@ export default React.createClass({
     console.log('header component rendering')
     return (
       <div id='header'>
-        <h1>Hello</h1>
-
-        <div id='todo_container'>
-            <h2>To Do List:</h2>
-            <table>
-                <tr>
-                    <th>placeholder</th>
-                </tr>
-                <tr>
-                    <td>heres an item</td>
-                </tr>
-            </table>
-        </div>
-
-        <div id='weather_container'>
-            <h2>Weather API</h2>
-            <p>it'll go here</p>
-        </div>
-
-        <div class='footer'>
-            <p>artist info</p>
-        </div>
+        // plan to implement this for each user + good *Morning* will change
+        // depending on the time
+        <h1>THE TIME</h1>
+        <h2>Good Morning, Devon</h2>
       </div>
     )
   }
